dae895219fb8a057a333917bca39ee4620908032
eman essam <emanessam05@gmail.com>
2023-03-17 12:03:10 +0000

    add Network Module and api provider

---
+++ app/src/main/java/com/perfectpresentation/forecastapp/di/NetworkModule.kt
@@ -0,0 +1,25 @@
+package com.perfectpresentation.forecastapp.di
+
+import android.content.Context
+import com.perfectpresentation.forecastapp.data.remote.ApiInterface
+import com.perfectpresentation.forecastapp.helper.RetrofitFactory
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+
+class NetworkModule {
+    @Module
+    @InstallIn(SingletonComponent::class)
+    object NetworkModule {
+        @Provides
+        fun provideForecastApi(
+            remoteDataSource: RetrofitFactory,
+            @ApplicationContext context: Context
+        ): ApiInterface {
+            return remoteDataSource.getService()
+        }
+
+    }
+}
